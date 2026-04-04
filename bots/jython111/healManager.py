35722b45a9c0e8139ed3548254f3e02ffa7bb59d
frkns <kennychau06@gmail.com>
2026-04-04 16:28:34 -0600

    add to gitignore

--- bots/jython111/healManager.py
+++
@@ -1,7 +0,0 @@
-from cambc import Controller, Position
-
-def tryHeal(ct: Controller, target: Position) -> bool:
-    if ct.can_heal(target):
-        ct.heal(target)
-        return True
-    return False
