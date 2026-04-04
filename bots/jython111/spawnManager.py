ef13ad40c2b157945d87c8119a23be97ff3c4660
frkns <kennychau06@gmail.com>
2026-04-04 17:39:12 -0600

    removed bots

--- bots/jython111/spawnManager.py
+++
@@ -1,9 +0,0 @@
-from cambc import Controller, Direction, Environment, Position
-
-from mapUtils import onTheMap
-
-def trySpawn(ct: Controller, position: Position) -> bool:
-    if ct.can_spawn(position):
-        ct.spawn_builder(position)
-        return True
-    return False
