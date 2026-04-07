af82ef8eb00603e302aae58c1409a14addce91f3
Matthew <64226660+MatthewWHuang@users.noreply.github.com>
2026-04-07 09:13:34 -0400

    Compiled latest

--- bots/latest/main.py
+++ bots/latest/main.py
@@ -1,4 +1,4 @@
-# latest,  @ 2026-04-07 09:10:45 (local)
+# latest,  @ 2026-04-07 09:12:53 (local)
 
 from __future__ import annotations
 from cambc import Team, EntityType, Direction, Position, ResourceType, Environment, GameConstants, GameError, Controller
