5e00fcc39d2ae21b0d2c3490f7d01c15e5e22460
Matthew <64226660+MatthewWHuang@users.noreply.github.com>
2026-04-06 20:14:03 -0400

    Fixed gunner building bugs

--- bots/latest/main.py
+++ bots/latest/main.py
@@ -1,4 +1,4 @@
-# latest,  @ 2026-04-06 19:15:48 (local)
+# latest,  @ 2026-04-06 20:13:48 (local)
 
 from __future__ import annotations
 from cambc import Team, EntityType, Direction, Position, ResourceType, Environment, GameConstants, GameError, Controller
@@ -180,7 +180,6 @@ class BfsBureau:
         cls.weight[idx + -56] += 1000000
         cls.weight[idx + -57] += 1000000
 
-        Debug.dot(Position(x, y), Color.YELLOW)
 
     @classmethod
     def remove_enemy_launcher(cls, idx):
@@ -221,7 +220,6 @@ class BfsBureau:
         if cls.weight[i] < 1:
             cls.weight[i] = 1
 
-        Debug.dot(Position(x, y), Color.GREEN)
 
 
     # works. but slow
@@ -867,7 +865,7 @@ class BfsBureau:
             return 1000000, None
 
         # ── Phase 2: bitmask BFS from Dijkstra frontier ──
-        Profiler.start()
+        
         _tb = _tx * stride + _ty
         _tm = 1 << _tb
         _uc = (cls.now_passable_int | _tm) & cls.board_mask
@@ -22551,7 +22549,6 @@ class HealTargeter:
         if best.building_heal + best.bot_heal < 4:
             return None
 
-        print(f'HealTargeter {best.position=} {best.building_heal=} {best.building_hp=}')
 
         return best.position
 
@@ -23687,9 +23684,9 @@ class MarketMaker:
 
     @staticmethod
     def harvester_cost(apos: Position) -> int:
-        Profiler.start()
+        
         bridges, _ = BfsBureau.find_bridge_route(apos, DarkForest.sink_set)
-        Profiler.end("""BfsBureau.find_bridge_route""")
+        
         h_cost, _ = Globals.ct.get_harvester_cost()
         b_cost, _ = Globals.ct.get_bridge_cost()
         return h_cost + b_cost * bridges
@@ -23709,7 +23706,7 @@ class MarketMaker:
             return False
 
         pbt = MarketMaker.harvester_payback(apos)
-        print(f"""{pbt=}""")
+        
 
         if int(pbt * 1.5 + 100) < Util.get_rounds_left():
             return True
@@ -24096,9 +24093,9 @@ class Pathfinder:
         Debug.line(target)
         my_pos = Globals.my_pos
 
-        Profiler.start()
+        
         dist, dir = BfsBureau.find_route(Globals.my_pos, target, ban_target_pos)
-        Profiler.end("""BfsBureau.find_route""")
+        
 
         if dir is None:
             cls.given_up = True
@@ -24180,8 +24177,6 @@ class Player:
             err = traceback.format_exc()
             Debug.tee(err)
             Debug.tee(f'(I am a {Globals.my_type})')
-
-            ct.resign()
 
 
 # ============================================================
@@ -24308,7 +24303,7 @@ class RouteToCore:
                 DarkForest.sink_set,
             )
 
-        print(f"""{bridge_dist=}""")
+        
 
         if first_target is None:
             Debug.tee("first_target is None: giving up")
@@ -24470,7 +24465,7 @@ class RouteToFoundry:
                 target_set,
             )
 
-        print(f"""{bridge_dist=}""")
+        
 
         if first_target is None:
             Debug.tee("RouteToFoundry: first_target is None, giving up")
@@ -26257,7 +26252,6 @@ class ShieldTargeter:
         if not best.harvester_adjacent:
             return None
 
-        print(f'ShieldTargetInfo {best.position=} {best.harvester_adjacent=}')
 
         return best.position
 
@@ -26447,7 +26441,6 @@ class StateFoundryBuild:
 class StateMoveTo:
     @classmethod
     def run(cls, pos, tag='_'):
-        print(f'{tag=}')
         Pathfinder.move_to(pos)
 
 
@@ -26566,9 +26559,9 @@ class Symmetry:
         cls.predict_enemy_core()
         DarkForest.register_enemy_core()
 
-        Profiler.start()
+        
         Map.sync_tile_infos()
-        Profiler.end_now("""Map.sync_tile_infos""")
+        
 
 
 
@@ -26841,10 +26834,10 @@ class TakedownTargetInfo:
 
     @staticmethod
     def is_better_than(a: TakedownTargetInfo, b: TakedownTargetInfo):
-        if a.has_transporter != b.has_transporter:
-            return a.has_transporter < b.has_transporter
         if a.enemy_turrets_nearby != b.enemy_turrets_nearby:
             return a.enemy_turrets_nearby > b.enemy_turrets_nearby
+        if a.has_transporter != b.has_transporter:
+            return a.has_transporter < b.has_transporter
         return a.dist_enemy_core < b.dist_enemy_core
 
 
@@ -26980,6 +26973,14 @@ class TurretTakedown:
             if not ti.harvester_adjacent: 
                 continue
 
+            if ti.has_building:
+                if not ti.is_building_ally:
+                    continue
+                if ti.entity_type != EntityType.ROAD:
+                    # We can build on top of allied transporters if we really need to
+                    if not ti.entity_type in Constants.TRANSPORTERS_SET:
+                        continue
+
             info = TakedownTargetInfo()
             cls.cand.append(info)
             info.position = pos
@@ -27078,9 +27079,9 @@ class Unit:
         Globals.start_tick()
         MarketMaker.refresh()
 
-        Profiler.start()
+        
         Map.fill_tile_info()
-        Profiler.end("""Map.fill_tile_info""")
+        
 
     @classmethod
     def run_turn(cls):
@@ -27089,7 +27090,7 @@ class Unit:
     @classmethod
     def end_turn(cls):
 
-        if Globals.round == 667:
+        if Globals.round == 1999:
             Profiler.report()
         print(f'scale ratio {MarketMaker.scale_ratio:.2f}')
 
@@ -27218,11 +27219,11 @@ class VisionTracker:
 
     @classmethod
     def canonical_ally(cls, from_pos: Position) -> BotInfo:
-        Profiler.start()
+        
         ret = min(cls.allies, key=
             lambda x: (Util.linf(from_pos, x.position) << 16) + x.id
         )
-        Profiler.end("""canonical_ally""")
+        
         return ret
 
 
@@ -27264,46 +27265,45 @@ class Builder(Unit):
     def start_turn(cls):
         Unit.start_turn()
 
-        Profiler.start()
+        
         BfsBureau.update()
-        Profiler.end("""BfsBureau.update""")
+        
 
         Symmetry.run_sym_check()
 
-        Profiler.start()
+        
         DarkForest.fcompute()
-        Profiler.end("""DarkForest.fcompute""")
+        
 
 
-        Profiler.start()
+        
         BfsBureau.bfs20()
-        Profiler.end("""BfsBureau.bfs20""")
+        
 
-        Profiler.start()
+        
         OreExecutive.fill()
-        Profiler.end("""OreExecutive.fill""")
+        
 
-        Profiler.start()
+        
         VisionTracker.fill()
-        Profiler.end("""VisionTracker.fill""")
+        
 
-        Profiler.start()
+        
         TurretTakedown.fill()
-        Profiler.end("""TurretTakedown.fill""")
+        
 
-        Profiler.start()
+        
         HarvesterAdjacent.fill()
-        Profiler.end("""HarvesterAdjacent.fill""")
+        
 
-        Profiler.start()
+        
         HealTargeter.fill()
-        Profiler.end("""HealTargeter.fill""")
+        
 
-        Profiler.start()
+        
         ShieldTargeter.fill()
-        Profiler.end("""ShieldTargeter.fill""")
+        
 
-        Symmetry.debug()
                 
 
 
@@ -27311,7 +27311,6 @@ class Builder(Unit):
     def run_turn(cls):
         cls.state, *args = cls.determine_state()
 
-        print(f'running: {cls.state}  @', *args, sep=' ')
 
         globals()[f'State{cls.state}'].run(*args)
 
@@ -27320,13 +27319,13 @@ class Builder(Unit):
     def end_turn(cls):
         Unit.end_turn()
 
-        Profiler.start()
+        
         HealExecutor.execute_heal_attempt()
-        Profiler.end("""HealExecutor.execute_heal_attempt""")
+        
 
-        Profiler.start()
+        
         Marker.attempt_mark()
-        Profiler.end("""Marker.attempt_mark""")
+        
 
 
 
@@ -27414,7 +27413,6 @@ class Core(Unit):
         Unit.start_turn()
         CoreHistory.fill()
         print(f'est income: {MarketMaker.est_income}')
-        print(f'sentinel cost: {Globals.ct.get_sentinel_cost()}')
 
     @classmethod
     def run_turn(cls):
@@ -27424,9 +27422,6 @@ class Core(Unit):
     @classmethod
     def end_turn(cls):
         Unit.end_turn()
-
-        if Globals.round > 666:
-            Globals.ct.resign()
 
 
 # ============================================================
